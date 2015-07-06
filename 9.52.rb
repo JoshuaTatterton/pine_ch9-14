6f793f7d6b9d25f8a6250a35200e249035abe459
Josh <Tattsy@hotmail.co.uk>
2015-07-06 15:55:43 +0100

    Update to 2nd task CH9

--- 9.52.rb
+++ 9.52.rb
@@ -1,26 +1,20 @@
 # This takes an inputted number and changes it to 
 # Old School roman numberals, max in the 1000's
 
-def divide x, y
-	z = [x / y, x % y]
+def roman x, y, z
+	print z * (x / y)
+	return x % y
 end
 
 puts "Please enter a number (between 1 and 4000) to be put into roman numerals"
-roman = gets.chomp.to_i
-
-M = divide roman, 1000
-print "M" * M[0]
-D = divide M[1], 500
-print "D" * D[0]
-C = divide D[1], 100
-print "C" * C[0]
-L = divide C[1], 50
-print "L" * L[0]
-X = divide L[1], 10
-print "X" * X[0]
-V = divide X[1], 5
-print "V" * V[0]
-I = divide V[1], 1
-print "I" * I[0]
+num = gets.chomp.to_i
+print "The old roman numeral of #{num} is "
+M = roman num, 1000, "M"
+D = roman M, 500, "D"
+C = roman D, 100, "C"
+L = roman C, 50, "L"
+X = roman L, 10, "X"
+V = roman X, 5, "V"
+I = roman V, 1, "I"
 puts
 	
